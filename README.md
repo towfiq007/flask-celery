d68c73a1262e48d2dc5dab9b8a89ce1f6e4234e8
towfiqpls <t.rahman@pipelinesecurity.net>
2023-01-18 12:52:03 +0600

    Test flask_socketIO

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Helping documents:
 ------------------
 https://blog.logrocket.com/optimizing-task-queues-celery-flask/
-
+https://docs.celeryq.dev/en/stable/getting-started/first-steps-with-celery.html
 
 
 
@@ -14,3 +14,11 @@ export FLASK_APP=app; python -m flask run
 
 terminal2:
 celery -A app.celery worker --loglevel=info
+
+
+First steps of celery:
+-----------------------
+Choosing and installing a message transport (broker).
+Installing Celery and creating your first task.
+Starting the worker and calling tasks.
+Keeping track of tasks as they transition through different states, and inspecting return values.
--- app.py
+++ app.py
@@ -1,49 +1,32 @@
-import time
-from flask import Flask, render_template
-
-# importing function to get celery app instance
-from celery_utils import get_celery_app_instance
+from flask import *  
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-app.jinja_env.auto_reload = True
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# celery app instance
-celery = get_celery_app_instance(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @socketio.on('client_event', namespace='/test')
+@socketio.on('client_event')
+def handle_my_custom_event(data_from_client):
+    print('client_event')
+    print(data_from_client)
+    emit('alert_events', 'Alert from server')
 
 
-# route that will show will simply render a HTML template
-@app.route("/tasks")
-def tasks():
-    return render_template("tasks.html")
+@socketio.on('alert_events')
+def handle_my_custom_event(data_from_client):
+    print(data_from_client)
+    emit('alert_events', 'Alert from server')
+    print('emitted alert_events')
+
+@app.route('/')  
+def index():  
+      return render_template('index.html')  
+
+if __name__ == '__main__':
+    socketio.run(app)
+    # app.run(debug=True)
 
 
-# route that will execute a long running task
-@app.route("/long_running_task")
-def long_running_task():
-    # time in seconds 
-    time_to_wait = 5
-    print(f"This task will take {time_to_wait} seconds to complete...")
-    time.sleep(time_to_wait) 
-    return f"<p>The task completed in {time_to_wait} seconds!"
 
-
-# celery tasks
-@celery.task
-def sending_email_with_celery(): # simulate. not real.
-    print("Executing Long running task : Sending email with celery...")
-    time.sleep(5)
-    print("Task complete!")
-
-
-# route to trigger celery task
-@app.route("/long_running_task_celery")
-def long_running_task_celery():
-    # function.delay() is used to trigger function as celery task
-    sending_email_with_celery.delay()
-    return f"Long running task trigged with Celery! Check terminal to see the logs..."
